4d3d2bd44fe0af0213f4aae3eab4cc23afc89aaf
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2018-10-22 14:51:53 -0400

    two new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -9,6 +9,10 @@ alias repo="cd $HOME/Repositories"
 alias art="php artisan"
 alias artcc='php artisan cache:clear'
 alias fresh="php artisan migrate:fresh --seed"
+alias tinker="php artisan tinker"
+
+# Wordplate
+alias wordplate="composer create-project wordplate/wordplate"
 
 # Others
 alias "opn"="subl ."
