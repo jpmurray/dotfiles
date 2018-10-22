402eb2ec34e700ee3e603546938422786734c4e5
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2018-10-22 16:25:06 -0400

    fun stuff from mpociot/dotfiles

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,13 @@
 # CLI Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias reloadcli="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 
 # Directories
 alias repo="cd $HOME/Repositories"
+alias dotfiles="cd $DOTFILES"
 
 # Laravel
 alias art="php artisan"
