88ef953cbedc9c63ba1136389a33a0d8b196b8bb
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2018-10-24 08:47:39 -0400

    clear alias

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,7 @@ alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias c="clear"
 
 # Edit my SSH hosts config in Sublime
 alias sshosts='subl ~/.ssh/config'
